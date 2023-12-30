96b5b2454a66a388fb656afcf2b66db3632d7854
RaquelLeao <vc.raquel@gmail.com>
2023-12-30 18:25:12 -0300

    Inheriting class and overriding InstalarAplicativo method in Nokia class

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,16 @@
+using System;
+
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // Herdar da classe "Smartphone"
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria){}
+        
+        // Sobrescrever o método "InstalarAplicativo" para o Nokia
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando aplicativo {nomeApp} no Nokia...");
+        }
     }
 }
